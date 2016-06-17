0c18fdcf266367bf5060445ee391fe54e7db4ead
Cooper LeBrun <cooperlebrun@gmail.com>
2016-06-17 15:24:22 -0700

    break up big spec file into correct spec paths for guard

---
+++ spec/lib/flowlink_data/objectbase_spec.rb
@@ -0,0 +1,47 @@
+require_relative '../../../lib/flowlink_data/objectbase'
+
+class TestBase < Flowlink::ObjectBase
+  def self.fields
+    [:foo, :bar]
+  end
+
+  def initialize
+    fields.each do |f|
+      self.class.send(:define_method, f.to_s) { 1 }
+    end
+  end
+  
+  def baz(n = 0)
+    n == 1
+  end
+end
+
+RSpec.describe Flowlink do
+  describe Flowlink::ObjectBase do
+    # A module to be included in host Flowlink::Product classes.
+
+    let(:base) { TestBase.new }
+
+    context '#to_message' do
+      subject { base.to_message }
+      let(:keys) { subject.keys }
+
+      it { is_expected.to be_kind_of(Hash) }
+
+      it 'keys are strings' do
+        expect(keys.all? { |k| k.kind_of?(String) }).to eq(true)
+      end
+
+      it 'has keys matching fields' do
+        expect(keys.sort).to eq(base.fields.map(&:to_s).sort)
+      end
+
+      it 'can give additional arguments to a method' do
+        # TODO: should this be array of arrays or hash :(
+        test = ->{ base.to_hash([[:baz, 1]]) }
+        expect { test.call }.to_not raise_error # is this argument a good test?
+        expect(test.call['baz']).to be true
+      end
+    end
+  end
+end
