e43c7ec8e11e68b00742af32d2be38f5e0913743
Cooper LeBrun <cooperlebrun@gmail.com>
2016-06-17 13:45:30 -0700

    add optional override arg to ObjectBase#to_hash

--- lib/flowlink_data/objectbase.rb
+++ lib/flowlink_data/objectbase.rb
@@ -1,11 +1,43 @@
+class FieldMethod
+  attr_accessor :method_name, :args
+
+  def self.multi_new(methods)
+    methods.map do |m|
+      m = [m].flatten
+      FieldMethod.new(m.shift, m)
+    end
+  end
+
+  def self.merge(overrides, original)
+    overrides.inject(original) { |a, e| e.merge(a) }
+  end
+
+  def initialize(method_name, *args)
+    @method_name = method_name.to_sym
+    @args        = args.to_a.flatten
+  end
+
+  def merge(list)
+    list.delete_if { |o_fm| o_fm.method_name == method_name }
+    list << self
+  end
+
+  def to_a
+    [method_name] + args
+  end
+end
+
 module Flowlink
   class ObjectBase
     # Base class for any Flowlink objects. Child classes should implement
     # self.fields internally.
 
-    def to_hash
-      # Put values of calling field methods on self into a hash
-      Hash[fields.map { |f| [f.to_s, send(f)] }]
+    def to_hash(*overrides)
+      overrides = FieldMethod.multi_new(overrides)
+      defaults  = FieldMethod.multi_new(fields)
+      f_methods = FieldMethod.merge(overrides, defaults)
+
+      Hash[f_methods.map { |f| [f.method_name.to_s, send(*f.to_a)] }]
     end
 
     alias_method to_message to_hash
