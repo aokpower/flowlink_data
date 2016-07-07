19e0e5aeb25d75334eae37215775c07fc68d61f2
Cooper LeBrun <cooperlebrun@gmail.com>
2016-07-06 19:32:51 -0700

    remove # TODO: backtest comment for FieldMethod

--- spec/lib/field_method_spec.rb
+++ spec/lib/field_method_spec.rb
@@ -1,7 +1,6 @@
 require_relative '../../lib/field_method'
 
 # TODO: change FieldMethod.new to take a hash
-# TODO: backtest
 # TODO: Overrides won't be used when calling field methods.
 #   Is this really a problem? If you want an individual field, you can 
 #   #to_hash[:field], and if you want to call a specific field you do
